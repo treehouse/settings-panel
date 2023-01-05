91418b77c59adc8535395ccd817ccf1ee92a75bf
dustinusey <dustinusey@me.com>
2023-01-05 12:58:32 -0600

    project starter files ready

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+const settingsData = [
+    {
+        title: 'auto save photos', 
+        uid: 'autoSavePhotos',
+        description: 'automatically save photos when uploading them to your profile.',
+        isChecked: false
+    },
+    {
+        title: 'location services', 
+        uid: 'locationServices',
+        description: 'allow the app to have access to your location to show you more relevant posts.',
+        isChecked: true
+    },
+    {
+        title: 'dark mode', 
+        uid: 'darkMode',
+        description: 'toggle a darker theme for the app. this makes the UI easier on the eyes in darker environments.',
+        isChecked: false
+    }
+];
