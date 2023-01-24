24d763da49d29fde3dd1deb370b2ea0d1534a0bd
dustinusey <dustinusey@me.com>
2023-01-24 16:32:01 -0600

    finished code for master file

--- master/scripts/app.js
+++ master/scripts/app.js
@@ -24,39 +24,18 @@ const settings = document.getElementById('settings');
 const settingsPanel = document.getElementById('settingsPanel');
 
 settingsData.forEach(setting => {
-    createSetting(setting);
+    settings.innerHTML +=
+    `
+    <div class="setting">
+        <label for="${setting.uid}">
+            <span>${setting.title}</span>
+            <span>${setting.description}</span>
+        </label>
+        <input type="checkbox" id="${setting.uid}">
+    </div>
+    `
 });
 
-
-function createSetting(data) {
-    let setting = document.createElement('div');
-    setting.classList = 'setting';
-
-    let label = document.createElement('label');
-    label.setAttribute('for', data.uid);
-    let title = document.createElement('span');
-    title.textContent = data.title;
-    let desc = document.createElement('span');
-    desc.textContent = data.description;
-    label.appendChild(title);
-    label.appendChild(desc);
-
-    let toggle = document.createElement('input');
-    toggle.type = 'checkbox';
-    toggle.id = data.uid;
-
-    if (data.isChecked) {
-        toggle.checked = true;
-    } else {
-        toggle.checked = false;
-    }
-
-    setting.appendChild(label);
-    setting.appendChild(toggle);
-
-    settings.appendChild(setting);
-
-}
 
 
 if (document.getElementById('darkMode')) {
